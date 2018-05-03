908fe8896258c34673705be1e9df417af828e93f
Radu Gasler <radu.gasler@gentlab.com>
2018-05-03 17:32:42 +0300

    Joined table inheritance.

--- sql/db.sql
+++ sql/db.sql
@@ -1,9 +1,17 @@
 drop database if exists heroes;
-create database heroes;
-use heroes;
-create table hero (
-  id int not null auto_increment primary key,
-  name varchar(30),
-  life int,
-  power int
-);
+  create database heroes;
+  use heroes;
+  create table hero (
+    id int not null auto_increment primary key,
+    name varchar(30),
+    life int,
+    power int
+  );
+  create table extraterrestrial_hero (
+     id int not null primary key,
+     residence_planet varchar(30)
+  );
+  create table local_hero (
+     id int not null primary key,
+     residence_city varchar(30)
+  );
